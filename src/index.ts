b9a4fe44a595d82fafd04e2d0b570d82ae1999db
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2024-06-13 23:54:39 +0200

    [UPDATE] added webuntis

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { WebUntis } from 'webuntis';
+
+const untis = new WebUntis('FTS-Villingen-Schwenningen', process.env.USERNAME || "", process.env.PASSWORD || "", 'arche.webuntis.com');
+
+await untis.login();
+const timetable = await untis.getOwnTimetableForToday();
+
+Bun.serve({
+    port: 3000,
+    fetch(req) {
+        return new Response(JSON.stringify(timetable), {
+            headers: {
+                'content-type': 'application/json',
+            },
+        });
+    },
+});
